c0bcf33fe08f0596ad5c24dc1bd4ecbddfe7fd4d
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2022-12-27 16:23:59 +0100

    .

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+//import { MatButtonModule } from '@angular/material'
+
+const MaterialComponents = [
+  
+]
+
+@NgModule({
+  imports: [],
+  exports: []
+})
+export class MaterialModule { }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { Routes } from '@angular/router';
 
 
+const pagesRoute : Routes = [
+  {path: 'home', component: HomeComponent}
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule
   ]
